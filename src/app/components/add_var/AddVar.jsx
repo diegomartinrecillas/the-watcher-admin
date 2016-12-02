bbfdf8fe743d58a1033b9d14cc02d8c159ad3d09
diegomartin <diegomartinrecillas@outlook.com>
2016-12-01 18:36:01 -0600

    dialog

--- src/app/components/add_var/AddVar.jsx
+++ src/app/components/add_var/AddVar.jsx
@@ -1,3 +1,4 @@
+import timezone from 'moment-timezone';
 // React
 import React from 'react';
 // Flux
@@ -5,9 +6,11 @@ import linkState from 'app/utils/onChangeHandlerFactory';
 import VariableActions from 'app/actions/VariableActions';
 import VariableStore from 'app/stores/VariableStore';
 // Material UI Components
-import SelectField from 'material-ui/SelectField';
+import Dialog from 'material-ui/Dialog';
+import AutoComplete from 'material-ui/AutoComplete';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import Divider from 'material-ui/Divider';
 import Card from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
@@ -57,50 +60,22 @@ const styles = {
     isAddingVarSuccess: {},
 }
 
-const zones = [
-    {"value":"1", "name":"UTC-12"},
-    {"value":"2", "name":"UTC-11"},
-    {"value":"3", "name":"UTC-10"},
-    {"value":"4", "name":"UTC-9"},
-    {"value":"5", "name":"UTC-8"},
-    {"value":"6", "name":"UTC-7"},
-    {"value":"7", "name":"UTC-6"},
-    {"value":"8", "name":"UTC-5"},
-    {"value":"9", "name":"UTC-4"},
-    {"value":"10", "name":"UTC-3"},
-    {"value":"11", "name":"UTC-2"},
-    {"value":"12", "name":"UTC-1"},
-    {"value":"13", "name":"UTC+0"},
-    {"value":"14", "name":"UTC+1"},
-    {"value":"15", "name":"UTC+2"},
-    {"value":"16", "name":"UTC+3"},
-    {"value":"17", "name":"UTC+4"},
-    {"value":"18", "name":"UTC+5"},
-    {"value":"19", "name":"UTC+6"},
-    {"value":"20", "name":"UTC+7"},
-    {"value":"21", "name":"UTC+8"},
-    {"value":"22", "name":"UTC+9"},
-    {"value":"23", "name":"UTC+10"},
-    {"value":"24", "name":"UTC+11"},
-    {"value":"25", "name":"UTC+12"},
-    {"value":"26", "name":"UTC+13"},
-    {"value":"27", "name":"UTC+14"}
-]
-
 export default class AddVar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectValue: null,
+            open: false,
             varKey: '',
             varName: '',
             varUnit: '',
             varDescription: '',
             varZone: '',
+            varTimezone: '',
             isAddingVar: false,
             isAddingVarSuccess: false,
             isAddingVarError: false,
-            addingVarErrorMessage: ''
+            addingVarErrorMessage: '',
+            errorMessage: ''
         }
     }
 
@@ -126,129 +101,166 @@ export default class AddVar extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-        if (this.state.selectValue !== null &&
-            this.state.varName !== '' &&
-            this.state.varUnit !== '' &&
-            this.state.varZone !== '' &&
-            this.state.varDescription !== '') {
-
-            let data = {};
-
-            data.varName = this.state.varName;
-            data.varUnit = this.state.varUnit;
-            data.varDescription = this.state.varDescription;
-            data.varZone = this.state.varZone;
-            data.varTimezone = zones[this.state.selectValue - 1].name;
-
-            VariableActions.dispatch('addNewVariable', data);
-        } else {
-            //alert('Por favor llena todos los campos');
+        if (this.state.varName !== '' && this.state.varUnit !== '' && this.state.varZone !== '' && this.state.varDescription !== '') {
+            if (timezone.tz.names().indexOf(this.state.varTimezone) !== -1) {
+                let data = {};
+                data.varName = this.state.varName;
+                data.varUnit = this.state.varUnit;
+                data.varDescription = this.state.varDescription;
+                data.varZone = this.state.varZone;
+                data.varTimezone = this.state.varTimezone;
+                VariableActions.dispatch('addNewVariable', data);
+            } else {
+                this.handleOpen('La zona horaria que seleccionaste no es válida, por favor selecciona una zona de la lista.');
+            }
         }
-
+        else {
+            this.handleOpen('Por favor asegúrate de que todos los campos esten llenos.');
+        }
         return false;
     }
 
-    handleSelect = (event, index, selectValue) => {
+    handleOpen = (error) => {
         this.setState({
-            selectValue: selectValue
+            open: true,
+            errorMessage: error
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false,
+            errorMessage: ''
+        });
+    }
+
+    handleTimezone = (value) => {
+        this.setState({
+            varTimezone: value
         });
     }
 
     render() {
+        const actions = [
+            <FlatButton
+                label="Ok"
+                primary={true}
+                onTouchTap={this.handleClose}
+                />
+        ];
         return (
-            <div style={styles.mainContainer}>
-                <div style={styles.innerContainer}>
-                    <Subtitle text='Nueva Variable'/>
-                    <form>
-                        <div style={styles.dataContainer}>
-                            <Card style={styles.alignment}>
-                                <section style={styles.legend}>
-                                    <TextField
-                                        floatingLabelText="Nombre"
-                                        hintText="Nombre de la variable"
-                                        underlineStyle={styles.underlineStyle}
-                                        fullWidth={true}
-                                        value={this.state.varName}
-                                        onChange={linkState(this,'varName')}/>
+            <div>
+                <Dialog
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}>
+                    {this.state.errorMessage}
+                </Dialog>
+                <div style={styles.mainContainer}>
+                    <div style={styles.innerContainer}>
+                        <Subtitle text='Nueva Variable'/>
+                        <form>
+                            <div style={styles.dataContainer}>
+                                <Card style={styles.alignment}>
+                                    <section style={styles.legend}>
+                                        <TextField
+                                            required={true}
+                                            floatingLabelText="Nombre"
+                                            hintText="Nombre de la variable"
+                                            underlineStyle={styles.underlineStyle}
+                                            fullWidth={true}
+                                            value={this.state.varName}
+                                            onChange={linkState(this,'varName')}/>
+                                    </section>
+                                    <Divider/>
+                                    <section style={styles.legend}>
+                                        <TextField
+                                            required={true}
+                                            floatingLabelText="Unidad"
+                                            hintText="Unidad de la variable"
+                                            underlineStyle={styles.underlineStyle}
+                                            fullWidth={true}
+                                            value={this.state.varUnit}
+                                            onChange={linkState(this,'varUnit')}/>
+                                    </section>
+                                    <Divider/>
+                                    <section style={styles.legend}>
+                                        <TextField
+                                            required={true}
+                                            floatingLabelText="Descripción"
+                                            hintText="Descripción de la variable"
+                                            underlineStyle={styles.underlineStyle}
+                                            fullWidth={true}
+                                            value={this.state.varDescription}
+                                            onChange={linkState(this,'varDescription')}/>
+                                    </section>
+                                    <Divider/>
+                                    <section style={styles.legend}>
+                                        <TextField
+                                            required={true}
+                                            floatingLabelText="Zona"
+                                            hintText="Zona de la variable"
+                                            underlineStyle={styles.underlineStyle}
+                                            fullWidth={true}
+                                            value={this.state.varZone}
+                                            onChange={linkState(this,'varZone')}/>
+                                    </section>
+                                    <Divider/>
+                                    <section style={styles.legend}>
+                                        <AutoComplete
+                                            required={true}
+                                            fullWidth={true}
+                                            underlineStyle={styles.underlineStyle}
+                                            filter={AutoComplete.caseInsensitiveFilter}
+                                            floatingLabelText="Timezone"
+                                            hintText="Timezone"
+                                            onUpdateInput={this.handleTimezone}
+                                            maxSearchResults={3}
+                                            dataSource={timezone.tz.names()}/>
+                                    </section>
+                                </Card>
+                            </div>
+                            {this.state.isAddingVar &&
+                                <section>
+                                    <p style={styles.isAddingVar}>Espere un momento...</p>
                                 </section>
-                                <Divider/>
-                                <section style={styles.legend}>
-                                    <TextField
-                                        floatingLabelText="Unidad"
-                                        hintText="Unidad de la variable"
-                                        underlineStyle={styles.underlineStyle}
-                                        fullWidth={true}
-                                        value={this.state.varUnit}
-                                        onChange={linkState(this,'varUnit')}/>
+                            }
+                            {this.state.isAddingVarError &&
+                                <section>
+                                    <p style={styles.addingVarErrorMessage}>{this.state.addingVarErrorMessage}</p>
                                 </section>
-                                <Divider/>
-                                <section style={styles.legend}>
-                                    <TextField
-                                        floatingLabelText="Descripción"
-                                        hintText="Descripción de la variable"
-                                        underlineStyle={styles.underlineStyle}
-                                        fullWidth={true}
-                                        value={this.state.varDescription}
-                                        onChange={linkState(this,'varDescription')}/>
+                            }
+                            {this.state.isAddingVarSuccess &&
+                                <section>
+                                    <p style={styles.isAddingVarSuccess}>Nueva variable agregada con éxito</p>
                                 </section>
-                                <Divider/>
-                                <section style={styles.legend}>
-                                    <TextField
-                                        floatingLabelText="Zona"
-                                        hintText="Zona de la variable"
-                                        underlineStyle={styles.underlineStyle}
-                                        fullWidth={true}
-                                        value={this.state.varZone}
-                                        onChange={linkState(this,'varZone')}/>
+                            }
+                            {this.state.isAddingVarSuccess &&
+                                <section>
+                                    <p style={styles.varKey}>{this.state.varKey}</p>
                                 </section>
-                                <Divider/>
-                                <section style={styles.legend}>
-                                    <SelectField
-                                        value={this.state.selectValue}
-                                        onChange={this.handleSelect}
-                                        floatingLabelText="Uso Horario"
-                                        underlineStyle={styles.underlineStyle}
-                                        fullWidth={true}
-                                        maxHeight={200}>
-                                        {zones.map((zone) => (
-                                            <MenuItem
-                                                key={zone.value}
-                                                value={zone.value}
-                                                label={zone.name}
-                                                primaryText={zone.name} />
-                                        ))}
-                                    </SelectField>
-                                </section>
-                            </Card>
-                        </div>
-                        {this.state.isAddingVar &&
-                            <section>
-                                <p style={styles.isAddingVar}>Espere un momento...</p>
-                            </section>
-                        }
-                        {this.state.isAddingVarError &&
-                            <section>
-                                <p style={styles.addingVarErrorMessage}>{this.state.addingVarErrorMessage}</p>
-                            </section>
-                        }
-                        {this.state.isAddingVarSuccess &&
-                            <section>
-                                <p style={styles.isAddingVarSuccess}>Nueva variable agregada con éxito</p>
-                            </section>
-                        }
-                        {this.state.isAddingVarSuccess &&
-                            <section>
-                                <p style={styles.varKey}>{this.state.varKey}</p>
-                            </section>
-                        }
-                        <RaisedButton
-                            type="submit"
-                            label="Agregar"
-                            primary={true}
-                            style={styles.button}
-                            onClick={this.handleSubmit}
-                            />
-                    </form>
+                            }
+                            {!this.state.isAddingVarSuccess &&
+                                <RaisedButton
+                                    type="submit"
+                                    label="Agregar"
+                                    primary={true}
+                                    style={styles.button}
+                                    onClick={this.handleSubmit}
+                                    />
+                            }
+                            {this.state.isAddingVarSuccess &&
+                                <RaisedButton
+                                    type="submit"
+                                    label="Listo"
+                                    primary={true}
+                                    style={styles.button}
+                                    onClick={this.goBack}
+                                    />
+                            }
+                        </form>
+                    </div>
                 </div>
             </div>
         );
